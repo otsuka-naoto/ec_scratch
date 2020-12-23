d9d80c9fd219a44eae7a4a7ac371cfd739372111
otsuka-naoto <naoto1891@gmail.com>
2020-12-23 21:44:09 +0900

    create input form.

--- test.php
+++ test.php
@@ -3,6 +3,16 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php $hour=date('H'); ?> 
+ <?php if(5<=$hour && $hour<10): ?> 
+    <p>good morning!</p>
+ <?php else: ?> 
+    <p>good afternoon!</p>
+ <?php endif; ?>
+ <form action="form.php" method="post">
+ let's write your name!
+ <input type="text" name="name"/>
+ <input type="submit" value="submit"/>
+ </form>
  </body>
 </html>
