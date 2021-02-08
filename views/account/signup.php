d57bd205c996ab2689abcfeee0b539da4af8c25b
otsuka-naoto <naoto1891@gmail.com>
2021-02-09 01:20:49 +0900

    vueファイル追加

--- views/account/signup.php
+++ views/account/signup.php
@@ -1,19 +1,32 @@
-<?php  $this->setLayoutVar('title','アカウント登録画面') ?> 
+<?php $this->setLayoutVar('title', 'アカウント登録画面') ?>
 <h2>アカウント登録画面</h2>
- <form action="<?php echo $base_url; ?>/account/register" method="post">
- </br>
- ID:<input type="text" name="name" value="<?php echo $user_name; ?>" />
- </br>
- Pass: <input type="text" name="pass"/>
- </br>
- <input type="submit" value="login"/>
-
-<div id="app-1">{{ message }}</div>  <!-- {{ message }} が Vueデータに置換される -->
-
+<form action="<?php echo $base_url; ?>/account/register" method="post">
+  <div id='app'>
+    <router-link to="/top">Id:</router-link><input type="text" name="name" value="<?php echo $user_name; ?>" />
+    </br>
+    <router-link to="/users">Pass:</router-link><input type="text" name="pass" />
+    <router-view></router-view>
+  </div>
+</form>
+<script src="https://unpkg.com/vue@2.5.17"></script>
+<script src="https://unpkg.com/vue-router@3.0.1"></script>
 <script>
-var app1 = new Vue({
-  el: '#app-1',                        /* #app-1 要素に対して Vue を適用する */
-  data: { message: 'Hello world!' }    /* message という名前のデータを定義する */
-})
+  var router = new VueRouter({
+    routes: [{
+        path: '/top',
+        component: {
+          template: '<div>you need to input a id.</div>'
+        }
+      },
+      {
+        path: '/users',
+        component: {
+          template: '<div>you need to input a password.</div>'
+        }
+      }
+    ]
+  })
+  var app = new Vue({
+    router: router
+  }).$mount('#app')
 </script>
- </form>
---
+++ vue/sfc.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1 class="greeting">greeting:{{ msg }}</h1>
+    <Content />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Content,
+  },
+  data() {
+    return { msg: "hello!" };
+  },
+};
+</script>
+<style scoped>
+.greeting {
+  color: #42b983;
+}
+</style>
